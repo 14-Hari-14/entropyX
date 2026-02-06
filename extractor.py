7b2fe64cc57333ec90fff340e5d0f4de6a5b6353
hari <nhari142004@gmail.com>
2026-02-06 10:59:14 +0530

    Optimized the shanon_helper function
    1. Added a safety check for empty files
    2. Reduced the amount of steps taken to calculate entropy
    3. Understood the different between counter[value] += 1 and update
    funciton

--- extractor.py
+++ extractor.py
@@ -4,23 +4,22 @@ import math
 def shanon_helper(filepath):
     counter = Counter()
     entropy = 0.0
+    
     # Calculate the frequency of each byte in the file
     with open (filepath, 'rb') as file:
         file_bytes = file.read()
         counter.update(file_bytes)
     
-    # Calculate the probability of each byte
     total_bytes = sum(counter.values())
-    # Dictionary Comprehension: {key: new_value for key, old_value in dictionary.items()}
-    probabilities = {byte: count / total_bytes for byte, count in counter.items()}
     
-    for prob in probabilities.values():
-        entropy += prob*math.log2(prob)
+    # Safety check for empty files 
+    if total_bytes == 0:
+        return 0.0
     
-    entropy = 0.0-entropy
-    
-    print(entropy)
+    # Calculate the probability of each byte and use it to calculate entropy of the file
+    for count in counter.values():
+        prob = count/total_bytes
+        entropy -= prob*math.log2(prob)
+        
     return entropy
 
-
-shanon_helper("/home/hari/Computer_Science/projects/entropyX/generate_benign.sh")

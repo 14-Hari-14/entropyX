83c13835b32958230d88ac1959cc92d7083e8582
hari <nhari142004@gmail.com>
2026-02-16 17:23:51 +0530

    updated bulk_benign to increase the number of installers downloaded for dataset

--- scripts/bulk_benign.py
+++ scripts/bulk_benign.py
@@ -1,138 +1,244 @@
 import subprocess
 import os
+import time  # optional: small delay to be nice to servers
 
 # --- CONFIG ---
-DOWNLOAD_DIR = os.path.normpath(os.path.expanduser("~/Desktop/HardBenign1"))
+DOWNLOAD_DIR = os.path.normpath(os.path.expanduser("~/Desktop/HardBenign"))  # or HardBenign if that's your folder
 os.makedirs(DOWNLOAD_DIR, exist_ok=True)
 
-# --- THE EXPANDED MANIFEST (250+ IDs) ---
-MANIFEST_OLD = [
-    # --- BROWSERS & INTERNET (High Entropy Installers) ---
+# Combined & expanded manifest – old kept, new ~120 added (focus on .exe/.msi heavy)
+OLD_MANIFEST = [
+    # Your original core ones (kept for completeness, winget skips if present)
     "Google.Chrome", "Mozilla.Firefox", "Brave.Brave", "Opera.Opera", "Vivaldi.Vivaldi",
-    "TorProject.TorBrowser", "Waterfox.Waterfox", "Microsoft.Edge", "Maxthon.Maxthon",
-    
-    # --- MEDIA & DESIGN (Heavy Resources) ---
-    "VideoLAN.VLC", "Audacity.Audacity", "Spotify.Spotify", "Handbrake.Handbrake", 
-    "OBSProject.ObsStudio", "GIMP.GIMP", "Inkscape.Inkscape", "Krita.Krita", 
-    "BlenderFoundation.Blender", "DigiKam.digiKam", "Adobe.Acrobat.Reader.64-bit",
-    "CDBurnerXP.CDBurnerXP", "Kodi.Kodi", "Plex.Plex", "Stremio.Stremio",
-    "foobar2000.foobar2000", "MusicBee.MusicBee", "IrfanView.IrfanView",
-    
-    # --- DEV TOOLS & LANGUAGES (Complex Linker Signatures) ---
-    "Microsoft.VisualStudioCode", "Git.Git", "Python.Python.3.12", "NodeJS.NodeJS.LTS", 
-    "GoLang.Go", "Rustlang.Rust.MSVC", "Docker.DockerDesktop", "PuTTY.PuTTY", 
-    "WinSCP.WinSCP", "FileZilla.FileZilla", "Oracle.JDK.21", "JetBrains.PyCharm.Community",
-    "JetBrains.IntelliJIDEA.Community", "Postman.Postman", "Arduino.IDE", 
-    "Hashicorp.Vagrant", "Terraform.Terraform", "GitHub.GitHubDesktop", "TortoiseGit.TortoiseGit",
-    "DBeaver.DBeaver", "MongoDB.Compass", "HeidiSQL.HeidiSQL", "SQLiteBrowser.SQLiteBrowser",
-    
-    # --- UTILITIES & SYSTEM (Varying Header Sizes) ---
+    "TorProject.TorBrowser", "Waterfox.Waterfox", "Microsoft.Edge",
+    "VideoLAN.VLC", "Audacity.Audacity", "Spotify.Spotify", "Handbrake.Handbrake",
+    "OBSProject.ObsStudio", "GIMP.GIMP", "Inkscape.Inkscape", "Krita.Krita",
+    "BlenderFoundation.Blender", "Adobe.Acrobat.Reader.64-bit",
+    "Microsoft.VisualStudioCode", "Git.Git", "Python.Python.3.12", "NodeJS.NodeJS.LTS",
+    "Docker.DockerDesktop", "PuTTY.PuTTY", "WinSCP.WinSCP", "FileZilla.FileZilla",
+    "JetBrains.PyCharm.Community", "JetBrains.IntelliJIDEA.Community", "Postman.Postman",
     "7zip.7zip", "RARLab.WinRAR", "Microsoft.PowerToys", "pbatard.Rufus", "CPUID.CPU-Z",
-    "Piriform.CCleaner", "Piriform.Speccy", "BleachBit.BleachBit", "GlarySoft.GlaryUtilities",
-    "CrystalDewWorld.CrystalDiskInfo", "CrystalDewWorld.CrystalDiskMark", "CPUID.HWMonitor",
-    "BleachBit.BleachBit", "WiseCleaner.WiseDiskCleaner", "IObit.DriverBooster",
-    "Sysinternals.ProcessExplorer", "Sysinternals.ProcessMonitor", "Sysinternals.Autoruns",
-    "KeePassX.KeePassX", "Bitwarden.Bitwarden", "StandardNotes.StandardNotes",
-    
-    # --- COMMUNICATION ---
-    "Discord.Discord", "SlackTechnologies.Slack", "Telegram.TelegramDesktop", "Zoom.Zoom", 
-    "Skype.Skype", "Microsoft.Teams", "Signal.Signal", "Element.Element", "WhatsApp.WhatsApp",
+    "Piriform.CCleaner", "BleachBit.BleachBit", "CrystalDewWorld.CrystalDiskInfo",
+    "Discord.Discord", "SlackTechnologies.Slack", "Telegram.TelegramDesktop", "Zoom.Zoom",
+    "Steam.Steam", "EpicGames.EpicGamesLauncher", "LibreOffice.LibreOffice", "Notepad++.Notepad++",
+    "AnyDesk.AnyDesk", "TeamViewer.TeamViewer",
 
-    # --- GAMING & RUNTIMES (Very Large Files) ---
-    "Steam.Steam", "EpicGames.EpicGamesLauncher", "GOG.Galaxy", "Ubisoft.Connect",
-    "Unity.UnityHub", "EpicGames.UnrealEngine", "Roblox.Roblox",
-    
-    # --- PRODUCTIVITY ---
-    "LibreOffice.LibreOffice", "SumatraPDF.SumatraPDF", "Notepad++.Notepad++", 
-    "Evernote.Evernote", "Logitech.Options", "Wacom.Tablet", "AutoHotkey.AutoHotkey",
-    "Toggl.TogglDesktop", "AnyDesk.AnyDesk", "TeamViewer.TeamViewer"
-    
-    # Note: Adding 'Microsoft.DotNet.Runtime.8' and 'Microsoft.DotNet.DesktopRuntime.8'
-    # provides many smaller .exe files with modern signatures.
+    # Previous additions you already had (kept, skips fast)
+    "Adobe.Acrobat.Reader.32-bit", "Anaconda.Anaconda3", "Balena.Etcher", "Bitwarden.Bitwarden",
+    "Citrix.Workspace", "Codeblocks.Codeblocks", "DotPDN.PaintDotNet", "EclipseAdoptium.Temurin.11",
+    "EclipseAdoptium.Temurin.17", "EclipseAdoptium.Temurin.8", "Foxit.FoxitPDFEditor",
+    "FreeCAD.FreeCAD", "Google.AndroidStudio", "Greenshot.Greenshot", "ImageMagick.ImageMagick",
+    "KeePassXCTeam.KeePassXC", "Kubernetes.minikube", "Malwarebytes.Malwarebytes",
+    "Microsoft.DotNet.SDK.8", "Microsoft.PowerShell", "Microsoft.SQLServerManagementStudio",
+    "Microsoft.VisualStudio.2022.BuildTools", "Mozilla.Thunderbird", "Notion.Notion",
+    "Obsidian.Obsidian", "qBittorrent.qBittorrent", "VeraCrypt.VeraCrypt",
+    "VMware.WorkstationPro", "WiresharkFoundation.Wireshark", "Zotero.Zotero",
+
+    # NEW ~120 high-yield additions (diverse categories, likely .exe/.msi in 2026)
+    "Amazon.AWSCLI", "Arduino.ArduinoIDE", "AutoHotkey.AutoHotkey", "BeyondCompare.BeyondCompare",
+    "CMake.CMake", "DBeaver.DBeaver", "Everything.VoidTools", "Fiddler.Everywhere",
+    "FileZilla.FileZilla.Server", "Ghisler.TotalCommander", "GitExtensions.GitExtensions",
+    "Google.EarthPro", "HashiCorp.Vagrant", "HeidiSQL.HeidiSQL", "Hugo.Hugo.Extended",
+    "Insomnia.Insomnia", "IrfanView.IrfanView", "JetBrains.CLion", "JetBrains.DataGrip",
+    "JetBrains.GoLand", "JetBrains.PhpStorm", "JetBrains.Rider", "JetBrains.RubyMine",
+    "JetBrains.WebStorm", "KDE.Okular", "Lenovo.Vantage", "Logitech.OptionsPlus",
+    "Microsoft.AzureDataStudio", "Microsoft.Bicep", "Microsoft.PowerToys", "Microsoft.Sysinternals.Autoruns",
+    "Microsoft.Sysinternals.ProcessExplorer", "Microsoft.Sysinternals.ProcessMonitor",
+    "Microsoft.WindowsTerminal", "MiniTool.PartitionWizard.Free", "MPC-HC.MPC-HC",
+    "Neovim.Neovim", "Nmap.Nmap", "Notion.Notion", "OhMyPosh.OhMyPosh",
+    "OpenSCAD.OpenSCAD", "Oracle.VirtualBox", "PDF24.PDF24Creator", "PaintDotNet.PaintDotNet",
+    "Pandoc.Pandoc", "PortSwigger.BurpSuite.Community", "PowerShell.PowerShell",
+    "PrivateInternetAccess.PrivateInternetAccess", "Proton.ProtonVPN", "PuTTY.PuTTYgen",
+    "Python.Launcher", "RadareOrg.Cutter", "RealVNC.VNCViewer", "Recuva.Recuva",
+    "Rufus.Rufus", "Rustlang.Rustup", "Samsung.Magician", "SublimeHQ.SublimeMerge",
+    "SublimeHQ.SublimeText.4", "Sysinternals.Autoruns", "Sysinternals.BgInfo",
+    "Sysinternals.DiskView", "Sysinternals.TCPView", "TablePlus.TablePlus",
+    "TechPowerUp.GPU-Z", "TeraCopy.TeraCopy", "Typora.Typora", "Ventoy.Ventoy",
+    "VisualStudioCode.VisualStudioCode.Insiders", "VoidTools.Everything",
+    "WinDirStat.WinDirStat", "WinMerge.WinMerge", "WinSCP.WinSCP", "XnView.XnView",
+    "Yarn.Yarn", "Zadig.Zadig", "balena.balenaEtcher", "dbeaver.dbeaver",
+    "drawio.drawio", "fastfetch.fastfetch", "freetube.Freetube", "golang.go",
+    "he3.app", "httpie.desktop", "hugo.extended", "insomnia.Insomnia",
+    "ionic.ionic", "jupyterlab-desktop.jupyterlab-desktop", "keepassxc.keepassxc",
+    "lazycat.lazycat", "lens.lens", "lunarclient.lunarclient", "mremoteng.mremoteng",
+    "neovide.neovide", "netbirdio.netbird", "ngrok.ngrok", "nvm.nvm",
+    "ollama.ollama", "openlens.openlens", "orbstack.orbstack", "picpick.picpick",
+    "postman.postman-agent", "rclone.rclone", "redis.redis", "restic.restic",
+    "rclone.rclone", "rustdesk.rustdesk", "starship.starship", "syncthing.syncthing",
+    "tailscale.tailscale", "tldr.tldr", "vagrant.vagrant", "vscodium.vscodium",
+    "wez.wezterm", "wingetcreate", "yt-dlp.yt-dlp", "zig.zig", "zoxide.zoxide",
+        # Dev & Tools
+    "JetBrains.CLion",
+    "JetBrains.DataGrip",
+    "JetBrains.GoLand",
+    "JetBrains.PhpStorm",
+    "JetBrains.Rider",
+    "JetBrains.RubyMine",
+    "JetBrains.WebStorm",
+    "JetBrains.Toolbox",
+    "SublimeHQ.SublimeText.4",
+    "SublimeHQ.SublimeMerge",
+    "Neovim.Neovim",
+    "VisualStudioCode.VisualStudioCode.Insiders",
+    "vscodium.vscodium",
+    "wez.wezterm",
+
+    # Utilities & System
+    "Ghisler.TotalCommander",
+    "WinDirStat.WinDirStat",
+    "TechPowerUp.GPU-Z",
+    "MiniTool.PartitionWizard.Free",
+    "PDF24.PDF24Creator",
+    "Typora.Typora",
+    "drawio.drawio",
+    "picpick.picpick",
+
+    # Security & VPN
+    "Proton.ProtonVPN",
+    "NordVPN.NordVPN",
+    "Surfshark.Surfshark",
+    "ExpressVPN.ExpressVPN",
+    "PrivateInternetAccess.PrivateInternetAccess",
+    "CyberGhost.CyberGhostVPN",
+
+    # Networking & Misc
+    "tailscale.tailscale",
+    "netbirdio.netbird",
+    "ngrok.ngrok",
+    "rclone.rclone",
+    "syncthing.syncthing",
+    "rustdesk.rustdesk",
+    "Nmap.Nmap",
+    "WiresharkFoundation.Wireshark",          # different version likely
+    "OpenVPNTechnologies.OpenVPNConnect",
+
+    # Creative & Media
+    "Wondershare.Filmora",
+    "DaVinciResolve.DaVinciResolve",
+    "Affinity.Designer",
+    "Affinity.Photo",
+    "Affinity.Publisher",
+
+    # Other high-entropy .exe candidates
+    "CMake.CMake",
+    "Everything.VoidTools",
+    "Fiddler.Everywhere",
+    "Hugo.Hugo.Extended",
+    "Insomnia.Insomnia",
+    "OhMyPosh.OhMyPosh",
+    "OpenSCAD.OpenSCAD",
+    "Ventoy.Ventoy",
+    "Zadig.Zadig",
+    "starship.starship",
+    "zoxide.zoxide",
+    "fastfetch.fastfetch",
+    "lazycat.lazycat",
+    "tldr.tldr",
+    "yt-dlp.yt-dlp",
+    "zig.zig",
 ]
 
 MANIFEST = [
-    # New additions (diverse, high-yield for EXE installers)
-    "Adobe.Acrobat.Reader.32-bit", "Adobe.CreativeCloud", "AdoptOpenJDK.OpenJDK.11", "AdoptOpenJDK.OpenJDK.8",
-    "AIMP.AIMP", "Amazon.AWSCLI", "Amazon.Corretto.11", "Amazon.Corretto.17", "Amazon.Corretto.8",
-    "Amazon.S3Browser", "Amazon.SamCli", "Anaconda.Anaconda3", "Apache.Maven", "Apache.Tomcat",
-    "ApacheDirectoryStudio.ApacheDirectoryStudio", "Avast.FreeAntivirus", "Azure.DataStudio",
-    "Balena.Etcher", "Blizzard.Battle.net", "Bitwarden.CLI", "BlenderFoundation.Blender.Benchmark",
-    "Canonical.Ubuntu.2004", "Canonical.Ubuntu.2204", "Canonical.Ubuntu.2404", "Cisco.WebexMeetings",
-    "Citrix.Workspace", "Clementine.Clementine", "Codeblocks.Codeblocks", "Corsair.iCUE.4",
-    "Corsair.iCUE.5", "DBBrowserForSQLite.DBBrowserForSQLite", "Devolutions.RemoteDesktopManagerFree",
-    "DotPDN.PaintDotNet", "EclipseAdoptium.Temurin.11", "EclipseAdoptium.Temurin.17", "EclipseAdoptium.Temurin.8",
-    "EclipseFoundation.Eclipse", "ElectronicArts.EADesktop", "Foxit.FoxitPDFEditor", "Foxit.FoxitPDFReader",
-    "FreeCAD.FreeCAD", "FreeDownloadManager.FDM", "GnuPG.Gpg4win", "Google.AndroidStudio",
-    "Google.Chrome.Beta", "Google.Chrome.Dev", "Google.Drive", "Google.EarthPro",
-    "Greenshot.Greenshot", "HandBrake.HandBrake.CLI", "Hashicorp.VirtualBox.ExtensionPack",
-    "IBM.Semeru.11", "IBM.Semeru.17", "IBM.Semeru.8", "IDM.InternetDownloadManager",
-    "ImageMagick.ImageMagick", "JetBrains.CLion", "JetBrains.DataGrip", "JetBrains.GoLand",
-    "JetBrains.PhpStorm", "JetBrains.Rider", "JetBrains.RubyMine", "JetBrains.WebStorm",
-    "KeePassXCTeam.KeePassXC", "Kitware.CMake", "Kubernetes.minikube", "Logitech.GHUB",
-    "Logitech.LogiTune", "Logitech.UnifyingSoftware", "MPC-BE.MPC-BE", "Malwarebytes.Malwarebytes",
-    "Mega.MEGASync", "Microsoft.AzureCLI", "Microsoft.AzureCosmosEmulator", "Microsoft.AzureFunctionsCoreTools",
-    "Microsoft.AzureStorageEmulator", "Microsoft.AzureStorageExplorer", "Microsoft.Bicep",
-    "Microsoft.BotFrameworkComposer", "Microsoft.BotFrameworkEmulator", "Microsoft.DotNet.SDK.3",
-    "Microsoft.DotNet.SDK.5", "Microsoft.DotNet.SDK.6", "Microsoft.DotNet.SDK.7", "Microsoft.DotNet.SDK.8",
-    "Microsoft.DotNet.SDK.Preview", "Microsoft.OneDrive", "Microsoft.PowerBI", "Microsoft.PowerShell",
-    "Microsoft.PowerShell.Preview", "Microsoft.RemoteDesktopClient", "Microsoft.SQLServerManagementStudio",
-    "Microsoft.VisualStudio.2022.BuildTools", "Microsoft.VisualStudio.2022.Community",
-    "Microsoft.VisualStudio.2022.Enterprise", "Microsoft.VisualStudio.2022.Professional",
-    "Microsoft.VisualStudio.Locator", "Microsoft.VisualStudioCode.Insiders", "Microsoft.WindowsSDK",
-    "Microsoft.WindowsTerminal.Preview", "Microsoft.WingetCreate", "Microsoft.XNARedist",
-    "Mozilla.Thunderbird", "NZXT.CAM", "Nasm.Nasm", "Nextcloud.NextcloudDesktop",
-    "Notion.Notion", "OBSProject.OBSStudio.Pre-release", "Obsidian.Obsidian",
-    "OpenJS.NodeJS.Nightly", "OpenVPNTechnologies.OpenVPN", "OpenVPNTechnologies.OpenVPNConnect",
-    "Oracle.JavaRuntimeEnvironment", "Oracle.MySQLWorkbench", "Oracle.VirtualBox.ExtensionPack",
-    "Pandoc.Pandoc", "Parsec.Parsec", "Piriform.Defraggler", "Piriform.Recuva",
-    "Plex.PlexMediaPlayer", "Plex.PlexMediaServer", "Postman.Postman.Canary",
-    "PrivateInternetAccess.PrivateInternetAccess", "ProtonTechnologies.ProtonVPN", "Python.Python.3.10",
-    "Python.Python.3.11", "Python.Python.3.9", "qBittorrent.qBittorrent", "Razer.Synapse3",
-    "RealVNC.VNCViewer", "RevoGroup.RevoUninstaller", "RevoGroup.RevoUninstallerPro",
-    "SCOOTERSoftware.BeyondCompare4", "SQLite.SQLite", "SublimeHQ.SublimeText.3",
-    "SublimeHQ.SublimeText.4", "SublimeHQ.SublimeText.Dev", "Symless.Synergy",
-    "TailwindLabs.TailwindCSS", "TechSmith.Snagit", "TeraCopy.TeraCopy",
-    "TheDocumentFoundation.LibreOffice", "Valve.SteamCMD", "Vercel.Hyper",
-    "VideoLAN.VLC.Nightly", "VMware.HorizonClient", "VMware.WorkstationPro",
-    "VeraCrypt.VeraCrypt", "Wagnardsoft.DisplayDriverUninstaller", "WiresharkFoundation.Wireshark",
-    "XnSoft.XnView", "Yarn.Yarn", "balena.balenaCLI", "dbgate.dbgate",
-    "eclipse.embed-cdt", "gitextensions.gitextensions", "jqlang.jq", "kubernetes-cli",
-    "msys2.msys2", "obsidianmd.obsidian-insider", "openscad.openscad", "portSwigger.burp.suite.community",
-    "radareorg.cutter", "rustdesk.rustdesk", "samsung.magician", "seafile.seafile-client",
-    "shininglight.openssl", "sqlitebrowser.sqlitebrowser", "strawberryperl.strawberryperl",
-    "sublimetext.sublime-merge", "tailwindlabs.tailwindcss", "telerik.fiddler.classic",
-    "temurin.temurin.18", "typora.typora", "ventoy.ventoy", "voidtools.Everything",
-    "whatsapp.whatsapp.beta", "winmerge.winmerge", "xnsoft.xnconvert", "xnsoft.xnviewmp",
-    "yubico.yubikey-manager", "zotero.zotero"
+    # More JetBrains (different editions/variants not in your list)
+    "JetBrains.Aqua",
+    "JetBrains.Datalore",
+    "JetBrains.Fleet",
+    "JetBrains.MPS",
+
+    # Editors & IDE alternatives
+    "Helix.Helix",
+    "Lapce.Lapce",
+    "Zed.Zed",
+
+    # File / System Utils
+    "JAMSoftware.TreeSize.Free",
+    "AntibodySoftware.Antibody",
+    "BulkRenameUtility.BulkRenameUtility",
+    "Files.Files",
+    "ShareX.ShareX",
+
+    # Security / Privacy / VPN extras
+    "MullvadVPN.MullvadVPN",
+    "Windscribe.Windscribe",
+    "Bitwarden.CLI",
+    "Yubico.YubiKeyManager",
+
+    # Media / Creative
+    "Kdenlive.Kdenlive",
+    "Shotcut.Shotcut",
+    "OliveTeam.Olive",
+    "Natron.Natron",
+
+    # Dev / CLI / Portable tools
+    " Nushell.Nushell",
+    "Eza-ls.Eza",
+    "Bat.Bat",
+    "Fd.Fd",
+    "Ripgrep.Ripgrep",
+    "Tokei.Tokei",
+    "Hexyl.Hexyl",
+    "Tokei.Tokei",
+
+    # Other high-entropy .exe heavy
+    "BleachBit.BleachBit",          # different version likely
+    "SumatraPDF.SumatraPDF",
+    "qBittorrent.qBittorrent",
+    "Calibre.Calibre",
+    "JDownloader.JDownloader",
+    "yt-dlp.yt-dlp",                 # new version if available
+    "yt-dlp.YtDlp",
+    "yt-dlp.YtDlp-nightly",
+    "Ventoy.Ventoy",                 # retry if missed
+    "Rufus.Rufus",
+    "Etcher.BalenaEtcher",
+    "GodotEngine.GodotEngine",
+    "GodotEngine.GodotMono",
+    "Blender.Benchmark",
+    "LMMS.LMMS",
+    "TuxGuitar.TuxGuitar",
+    "MuseScore.MuseScore",
+    "Avidemux.Avidemux",
+    "LosslessCut.LosslessCut",
+    "HandBrake.HandBrake.Nightly",
+    "MPC-BE.MPC-BE",
+    "SMPlayer.SMPlayer",
+    "VLC.VLC.Nightly",
+    "Inkscape.Inkscape",
+    "Krita.Krita.Beta",
+    "GnuCash.GnuCash",
+    "MoneyManagerEx.MoneyManagerEx",
+    "HomeBank.HomeBank",
+    "Stellarium.Stellarium",
+    "Marble.Marble",
+    "Celestia.Celestia"
 ]
 
 def run_harvest():
     success_count = 0
     total = len(MANIFEST)
-    print(f"[*] Attempting to fetch {total} packages...")
-
+    print(f"[*] Attempting to fetch/check {total} packages...")
     for i, pkg_id in enumerate(MANIFEST):
-        print(f"[{i+1}/{total}] Fetching: {pkg_id}")
-        
+        print(f"[{i+1}/{total}] Processing: {pkg_id}")
         cmd = [
             "winget", "download", "--id", pkg_id,
             "--download-directory", DOWNLOAD_DIR,
             "--accept-source-agreements",
             "--accept-package-agreements"
         ]
-        
         try:
-            # Skipping output to keep terminal clean
-            res = subprocess.run(cmd, capture_output=True, text=True, timeout=120)
+            res = subprocess.run(cmd, capture_output=True, text=True, timeout=180)  # increased timeout for large files
             if res.returncode == 0:
                 success_count += 1
+                print(f"   [+] Success: {pkg_id}")
             else:
-                print(f"    [!] Skip: {pkg_id} (Package unavailable or Store-only)")
-        except:
-            continue
-
-    print(f"\n[FINISH] Success: {success_count} installers saved to {DOWNLOAD_DIR}")
+                print(f"   [!] Skip/Fail: {pkg_id} (code {res.returncode})")
+            time.sleep(1)  # gentle rate limit
+        except subprocess.TimeoutExpired:
+            print(f"   [!] Timeout: {pkg_id} – consider manual download later")
+        except Exception as e:
+            print(f"   [!] Error: {pkg_id} → {e}")
+    print(f"\n[FINISH] New successes this run: {success_count}")
+    print(f"Check folder for total (should exceed 200 now).")
 
 if __name__ == "__main__":
     run_harvest()
--- scripts/merge_datasets.py
+++ scripts/merge_datasets.py
@@ -1,14 +1,15 @@
 import pandas as pd
 
 # Load both datasets
-df1 = pd.read_csv('../data/dataset_v1.csv')
+df1 = pd.read_csv('../data/dataset_benign_v2.csv')
 df2 = pd.read_csv('../data/dataset_ransomware.csv')
+df3 = pd.read_csv('../data/dataset_ransomware_custom.csv')
 
 print(f"Dataset 1: {len(df1)} rows")
 print(f"Dataset 2: {len(df2)} rows")
-
+print(f"Dataset 3: {len(df3)} rows")
 # Merge (concatenate)
-merged = pd.concat([df1, df2], ignore_index=True)
+merged = pd.concat([df1, df2, df3], ignore_index=True)
 
 # Shuffle
 merged = merged.sample(frac=1, random_state=42).reset_index(drop=True)
@@ -16,5 +17,5 @@ merged = merged.sample(frac=1, random_state=42).reset_index(drop=True)
 print(f"Merged & Shuffled: {len(merged)} rows")
 
 # Save
-merged.to_csv('../data/dataset_merged.csv', index=False)
-print("Saved to ../data/dataset_merged.csv")
+merged.to_csv('../data/dataset_merged_v2.csv', index=False)
+print("Saved to ../data/dataset_merged_v2.csv")
